741b415d70a48ae5bad9d00d50886618f32db623
kmkruger <kmkruger2@wisc.edu>
2023-03-16 10:10:06 -0500

    models updates

--- inflammation/models.py
+++ inflammation/models.py
@@ -97,3 +97,44 @@ def daily_std_dev(data):
     :param threshold: a start point for each patient to compare against each other
     :returns: a std. dev between patients """
     return np.std(data, axis = 1)
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Patient:
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        self.name = name
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] +1
+            except IndexError:
+                day = 0
+
+        new_observation = {
+            'day': day,
+            'value': value,
+        }
+
+        self.observations.append(new_observation)
+        return new_observation
+    def __str__(self):
+        return self.name
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+alice = Patient('Alice')
+
+obs = alice.add_observation(3)
+
+print(obs)
+
